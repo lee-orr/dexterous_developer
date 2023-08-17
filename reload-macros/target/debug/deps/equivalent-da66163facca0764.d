8abaf12fd6ed607422e8d1f29a0cc9644ee24516
lee-orr <lee-orr@users.noreply.github.com>
2023-08-17 15:26:35 -0400

    carg
    Signed-off-by: lee-orr <lee-orr@users.noreply.github.com>

--- reload-macros/target/debug/deps/equivalent-da66163facca0764.d
+++
@@ -1,5 +0,0 @@
-e:\bevy-ecs-ui-experiment\macros\target\debug\deps\equivalent-da66163facca0764.rmeta: C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\equivalent-1.0.1\src\lib.rs
-
-e:\bevy-ecs-ui-experiment\macros\target\debug\deps\equivalent-da66163facca0764.d: C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\equivalent-1.0.1\src\lib.rs
-
-C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\equivalent-1.0.1\src\lib.rs:
--- reload-macros/target/debug/deps/once_cell-a72a2adc0d81f3be.d
+++
@@ -1,7 +0,0 @@
-e:\bevy-ecs-ui-experiment\macros\target\debug\deps\once_cell-a72a2adc0d81f3be.rmeta: C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\once_cell-1.18.0\src\lib.rs C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\once_cell-1.18.0\src\imp_std.rs C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\once_cell-1.18.0\src\race.rs
-
-e:\bevy-ecs-ui-experiment\macros\target\debug\deps\once_cell-a72a2adc0d81f3be.d: C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\once_cell-1.18.0\src\lib.rs C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\once_cell-1.18.0\src\imp_std.rs C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\once_cell-1.18.0\src\race.rs
-
-C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\once_cell-1.18.0\src\lib.rs:
-C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\once_cell-1.18.0\src\imp_std.rs:
-C:\Users\Lee-Orr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\once_cell-1.18.0\src\race.rs:
