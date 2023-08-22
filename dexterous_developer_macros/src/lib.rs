a3fc1478ac00010f0998692a0f9003e3fb8e81f6
lee-orr <lee-orr@users.noreply.github.com>
2023-08-22 18:11:26 -0400

    clippy
    Signed-off-by: lee-orr <lee-orr@users.noreply.github.com>

--- dexterous_developer_macros/src/lib.rs
+++ dexterous_developer_macros/src/lib.rs
@@ -4,7 +4,7 @@ extern crate quote;
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{parse::Parse, parse_macro_input, punctuated::Punctuated, Expr, ExprPath, ItemFn, Token};
+use syn::{parse_macro_input, ItemFn};
 
 #[proc_macro_attribute]
 pub fn dexterous_developer_setup(_attr: TokenStream, item: TokenStream) -> TokenStream {
