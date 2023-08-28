5b696e799c42f3017e48c73b4f0b6ff23fb9d29e
lee-orr <lee-orr@users.noreply.github.com>
2023-08-28 23:17:02 +0000

    clippy + fmt
    Signed-off-by: lee-orr <lee-orr@users.noreply.github.com>

--- dexterous_developer/src/lib.rs
+++ dexterous_developer/src/lib.rs
@@ -2,4 +2,5 @@ pub use dexterous_developer_internal::*;
 
 #[cfg(feature = "hot_internal")]
 #[allow(unused_imports)]
+#[allow(clippy::single_component_path_imports)]
 use dexterous_developer_dynamic;
