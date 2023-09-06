42603fddb9b7aa3c69df69e0741b1af18d313666
lee-orr <lee-orr@users.noreply.github.com>
2023-09-06 14:01:56 +0000

    use a different dir
    Signed-off-by: lee-orr <lee-orr@users.noreply.github.com>

--- dexterous_developer_cli/src/paths.rs
+++ dexterous_developer_cli/src/paths.rs
@@ -7,7 +7,7 @@ pub fn get_paths() -> anyhow::Result<CliPaths> {
     let dirs = ProjectDirs::from("git", "dexterous_developer", "dexterous_developer_cli")
         .context("Couldn't get application directories")?;
 
-    let data = dirs.data_dir().to_path_buf();
+    let data = dirs.preference_dir().to_path_buf();
 
     if !data.exists() {
         println!("Setting up data directory at {data:?}");
