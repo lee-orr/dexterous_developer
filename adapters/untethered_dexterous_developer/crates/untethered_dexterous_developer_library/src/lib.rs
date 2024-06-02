be1391cafc450c4df28c8fddd74a81b279275f61
Lee-Orr <leeorr@gmail.com>
2024-06-02 17:05:09 +0000

    clippy + fmt

--- adapters/untethered_dexterous_developer/crates/untethered_dexterous_developer_library/src/lib.rs
+++ adapters/untethered_dexterous_developer/crates/untethered_dexterous_developer_library/src/lib.rs
@@ -1,24 +1,44 @@
 pub mod macros;
 
-use serde::{de::DeserializeOwned, Serialize};
+use std::marker::PhantomData;
 
-#[derive(Default)]
+use serde::{de::DeserializeOwned, Serialize};
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
+
 pub struct ReloadableMain<
+    Events,
     SerializableState: Serialize + DeserializeOwned + Default,
     SharedState: Default,
 > {
     pub serializable: SerializableState,
     pub shared: SharedState,
+    pub sender: UnboundedSender<Events>,
+    pub receiver: UnboundedReceiver<Events>,
+    phantom: PhantomData<Events>,
 }
 
-impl<SerializableState: Serialize + DeserializeOwned + Default, SharedState: Default>
-    ReloadableMain<SerializableState, SharedState>
+impl<Events, SerializableState: Serialize + DeserializeOwned + Default, SharedState: Default>
+    Default for ReloadableMain<Events, SerializableState, SharedState>
 {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
+impl<Events, SerializableState: Serialize + DeserializeOwned + Default, SharedState: Default>
+    ReloadableMain<Events, SerializableState, SharedState>
+{
+    pub fn new() -> Self {
+        let (sender, receiver) = unbounded_channel();
 
-#[cfg(test)]
-mod test {
-    #[tokio::test]
-    async fn 
+        Self {
+            serializable: Default::default(),
+            shared: Default::default(),
+            sender,
+            receiver,
+            phantom: PhantomData,
+        }
+    }
+
+    pub async fn run_loop() {}
 }
