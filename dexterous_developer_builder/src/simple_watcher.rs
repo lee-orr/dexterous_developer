466d014faa4518e367d2d3b7be677c5a1758c29d
Lee-Orr <leeorr@gmail.com>
2024-06-02 00:41:06 +0000

    add watcher tests

--- dexterous_developer_builder/src/simple_watcher.rs
+++ dexterous_developer_builder/src/simple_watcher.rs
@@ -261,3 +261,97 @@ fn gather_directory_content(
 
     Ok(result.collect())
 }
+
+#[cfg(test)]
+mod test {
+    use std::time::Duration;
+
+    use super::*;
+    use test_temp_dir::test_temp_dir;
+    use tokio::fs::*;
+    use tokio::io::AsyncWriteExt;
+    use tokio::sync::mpsc::*;
+    use tokio::sync::mpsc::error::TryRecvError;
+    use tokio::time::timeout;
+
+    #[tokio::test]
+    async fn watcher_can_see_changes_in_a_code_directory() {
+        let dir = test_temp_dir!();
+
+        let watcher = SimpleWatcher::default();
+
+        let (tx, mut rx) = unbounded_channel();
+
+        watcher.watch_code_directories(&[Utf8PathBuf::from_path_buf(dir.as_path_untracked().to_path_buf()).unwrap()], (0, tx)).expect("Couldn't set up watcher on temporary directory");
+
+        let result = rx.try_recv().expect_err("Should be empty");
+        
+        assert!(matches!(result, TryRecvError::Empty));
+
+        let _ = File::create(dir.as_path_untracked().join("test.txt")).await.expect("Couldn't create file");
+
+        let result = timeout(Duration::from_millis(10), rx.recv()).await.expect("Didn't recieve watcher message on time").expect("Didn't recieve watcher message");
+
+        assert!(matches!(result, BuilderIncomingMessages::CodeChanged));
+    }
+
+    #[tokio::test]
+    async fn watcher_provides_initial_files_in_asset_directory() {
+        let dir = test_temp_dir!();
+
+        let watcher = SimpleWatcher::default();
+
+        let (tx, mut rx) = unbounded_channel();
+
+        let _ = File::create(dir.as_path_untracked().join("test.txt")).await.expect("Couldn't create file");
+
+        watcher.watch_asset_directories(&[Utf8PathBuf::from_path_buf(dir.as_path_untracked().to_path_buf()).unwrap()], (0, tx)).expect("Couldn't set up watcher on temporary directory");
+        
+        let result = timeout(Duration::from_millis(10), rx.recv()).await.expect("Didn't recieve initial asset message on time").expect("Didn't recieve initial asset message");
+
+        let BuilderIncomingMessages::AssetChanged(record) = result else {
+            panic!("Got Message that isn't Asset Changed");
+        };
+
+        assert_eq!(record.name, "test.txt");
+    }
+
+    #[tokio::test]
+    async fn watcher_provides_changed_files_in_asset_directory() {
+        let dir = test_temp_dir!();
+
+        let watcher = SimpleWatcher::default();
+
+        let (tx, mut rx) = unbounded_channel();
+
+        let _ = File::create(dir.as_path_untracked().join("test.txt")).await.expect("Couldn't create file");
+
+        watcher.watch_asset_directories(&[Utf8PathBuf::from_path_buf(dir.as_path_untracked().to_path_buf()).unwrap()], (0, tx)).expect("Couldn't set up watcher on temporary directory");
+        
+        let result = timeout(Duration::from_millis(10), rx.recv()).await.expect("Didn't recieve initial asset message on time").expect("Didn't recieve initial asset message");
+
+        let BuilderIncomingMessages::AssetChanged(record) = result else {
+            panic!("Got Message that isn't Asset Changed");
+        };
+
+        let hash = record.hash.clone();
+
+        assert_eq!(record.name, "test.txt");
+
+        let mut file = File::create(dir.as_path_untracked().join("test.txt")).await.expect("Couldn't create file");
+
+        let _ = timeout(Duration::from_millis(10), rx.recv()).await.expect("Didn't recieve initial asset message on time").expect("Didn't recieve initial asset message");
+
+        file.write_all(b"my file").await.expect("Failed to write file");
+        
+        let result = timeout(Duration::from_millis(10), rx.recv()).await.expect("Didn't recieve initial asset message on time").expect("Didn't recieve initial asset message");
+
+        let BuilderIncomingMessages::AssetChanged(record) = result else {
+            panic!("Got Message that isn't Asset Changed");
+        };
+
+        assert_eq!(record.name, "test.txt");
+
+        assert!(hash != record.hash);
+    }
+}
