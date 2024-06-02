5a496cf3ddcc9ca38f821eebc7d68de9326037c7
Lee-Orr <leeorr@gmail.com>
2024-06-02 00:16:05 +0000

    clean up dexterous_developer crate

--- dexterous_developer/src/lib.rs
+++ dexterous_developer/src/lib.rs
@@ -1,7 +1,2 @@
 #[cfg(feature = "bevy")]
 pub use bevy_dexterous_developer::*;
-
-#[cfg(feature = "hot_internal")]
-#[allow(unused_imports)]
-#[allow(clippy::single_component_path_imports)]
-use dexterous_developer_dynamic;
