37dd66b3afe767e894d345e504b184ce69703991
Lee-Orr <leeorr@gmail.com>
2024-06-22 21:18:09 +0000

    temporarily remove xilem adapter

--- adapters/xilem_dexterous_developer/crates/xilem_dexterous_developer_dynamic/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![allow(unused_imports)]
-#![allow(clippy::single_component_path_imports)]
-
-pub use dexterous_developer_internal;
-pub use dexterous_developer_types;
-pub use xilem;
-pub use xilem_dexterous_developer_library;
--- adapters/xilem_dexterous_developer/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub use xilem_dexterous_developer_library::*;
-
-#[cfg(feature = "hot_internal")]
-#[allow(unused_imports)]
-#[allow(clippy::single_component_path_imports)]
-use xilem_dexterous_developer_dynamic;
