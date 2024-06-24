ea1138d1d78cc2b0c28bdf91f662cbfaf4b4ec3b
lee-orr <leeorr@gmail.com>
2024-06-24 02:14:54 +0000

    build all examples & binaries before test

--- adapters/bevy_dexterous_developer/tests/simple_cli_test.rs
+++ adapters/bevy_dexterous_developer/tests/simple_cli_test.rs
@@ -2,4 +2,5 @@
 
 #[tokio::test]
 async fn simple_cli_can_run() {
+    setup_test("simple_cli", |comms, send, output| {})
 }
--- dexterous_developer_test_utils/src/lib.rs
+++ dexterous_developer_test_utils/src/lib.rs
@@ -13,15 +13,25 @@ pub enum OutMessage {
     Exit(ExitStatus)
 }
 
-pub async fn setup_test(test_example: impl ToString, control: impl FnOnce(TestBuilderComms, mpsc::Sender<String>, mpsc::Receiver<OutMessage>)) {
+pub async fn setup_test(test_example: impl ToString) -> (TestBuilderComms, mpsc::UnboundedSender<String>, mpsc::UnboundedReceiver<OutMessage>) {
 
     let (builder, mut comms) = TestBuilder::new(None, None);
     let manager = dexterous_developer_manager::Manager::default().add_builders(&[Arc::new(builder)]).await;
     let (port_tx, port_rx) = tokio::sync::oneshot::channel();
+    
     tokio::spawn(async move {
         run_test_server(0, manager, port_tx).await.unwrap();
     });
 
     let port = port_rx.await.unwrap();
     comms.set_new_library(test_example.to_string());
+
+    let (command_tx, command_rx) = mpsc::unbounded_channel();
+    let (out_tx, out_rx) = mpsc::unbounded_channel();
+
+    tokio::spawn(async move {
+        
+    });
+
+    (comms, command_tx, out_rx)
 }
