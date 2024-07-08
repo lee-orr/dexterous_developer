bceb4366f225dff9079ac7742a28a9e5c3a1664e
lee-orr <leeorr@gmail.com>
2024-07-08 23:50:25 +0000

    cleanup sample

--- adapters/dioxus_dexterous_developer/examples/dioxus-desktop.rs
+++ adapters/dioxus_dexterous_developer/examples/dioxus-desktop.rs
@@ -12,47 +12,35 @@ reloadable_app!(app {
     let mut names = use_signal(|| vec!["John".to_string(), "Doe".to_string()]);
 
     dioxus::desktop::window().webview.zoom(2.0);
-    // dioxus::desktop::window().webview.zoom(1.0);
-
-    println!("rendering1a12s23asdkassssdaas3sad");
 
     //
     rsx! {
-        // Counter { id: 865 }
-        li { background_color: "red", "That's not good :/: {value}" }
         li { background_color: "green", "This is value: {value}" }
-        li { "This123usde123123: {value}" }
-        li { "This issd vaasdsdasdadsasdlue: 123{value}" }
-        li { "This is valsdue: {value}" }
         ul {
-            for item in 0..2 {
-                div {
-                    li { "Some123 thats crazy w123123orks?!121233  {item}" }
-                    li { "Some123 thats crazy w123123orks?!121233  {item}" }
-                    li { "Some123 thats crazy w123123orks?!121233  {item}" }
-                }
+            for item in 0..count() {
+                li { "{item}" }
             }
             br {}
             button {
                 onclick: move |_| {
                     count += 1;
-                    names.push("Newashdkjassh 3 Name".to_string());
-                    names.push("Newashdkjash 1 Name".to_string());
+                    names.push(" 3 Name".to_string());
+                    names.push(" 1 Name".to_string());
                 },
                 "Icrement"
             }
-            for name in names.iter() {
-                li { "{name}" }
-            }
             button {
                 onclick: move |_| {
-                    names.push("Newashdkjash 123 Name".to_string());
+                    names.push(" 123 Name".to_string());
                     names.clear();
                     count -= 1;
                 },
                 "Decremenet"
             }
             "Count {count}"
+            for name in names.iter() {
+                li { "{name}" }
+            }
         }
     }
 });
