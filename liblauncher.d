f2862f542c9a6918d45a5220d351fc003b1ca518
lee-orr <leeorr@gmail.com>
2024-08-16 01:42:49 +0000

    remove accidental build artifacts

--- liblauncher.d
+++
@@ -1,5 +0,0 @@
-liblauncher: adapters/bevy_dexterous_developer/examples/launcher.rs
-
-liblauncher.d: adapters/bevy_dexterous_developer/examples/launcher.rs
-
-adapters/bevy_dexterous_developer/examples/launcher.rs:
